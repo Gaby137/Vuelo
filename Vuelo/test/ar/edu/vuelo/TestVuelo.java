b140fe38546f4dab7727e7e4d4c28c8bdbe2938f
rrondosa <1017.reyna@gmail.com>
2022-05-11 13:42:24 -0300

    Se agrega el test1 para cmomprar un pasaje

--- Vuelo/test/ar/edu/vuelo/TestVuelo.java
+++ Vuelo/test/ar/edu/vuelo/TestVuelo.java
@@ -1,5 +1,20 @@
 package ar.edu.vuelo;
 
-public class TestVuelo {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import org.junit.Test;
+
+public class TestVuelo {
+	@Test
+	public void QueSePuedaCompraparUnPasajeParaPasajeroEnUnVuelo() {
+		
+		Vuelo v1 = new Vuelo("Buenos Aires","Madrid",10);
+		
+		Pasajero p1= new Pasajero(1,"nombre","apellido");
+		
+		p1.comprarPasaje(v1,5);
+		
+		assertEquals(p1, v1.getAsiento(p1.getAsiento().getNro()));
+	}
 }
